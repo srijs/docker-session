b2f9be997a054b2fe2acbd6eecf795fb752807d0
Sam Rijs <srijs@airpost.net>
2015-04-03 15:25:35 +1100

    go fmt

--- session.go
+++ session.go
@@ -1,102 +1,102 @@
 package session
 
 import (
-  "github.com/fsouza/go-dockerclient"
-  "io"
-  "os"
+	"github.com/fsouza/go-dockerclient"
+	"io"
+	"os"
 )
 
 type Session struct {
-  client *docker.Client
-  container *docker.Container
+	client    *docker.Client
+	container *docker.Container
 }
 
 type Options struct {
-  Image string
+	Image string
 }
 
 type Cmd struct {
-  Path string
-  Args []string
-  Stdin io.Reader
-  Stdout io.Writer
-  Stderr io.Writer
+	Path   string
+	Args   []string
+	Stdin  io.Reader
+	Stdout io.Writer
+	Stderr io.Writer
 }
 
 func NewSession(sopts Options) (*Session, error) {
 
-  host := os.Getenv("DOCKER_HOST")
-  if host == "" {
-    host = "unix:///var/run/docker.sock"
-  }
+	host := os.Getenv("DOCKER_HOST")
+	if host == "" {
+		host = "unix:///var/run/docker.sock"
+	}
 
-  client, err := docker.NewClient(host)
-  if err != nil {
-    return nil, err
-  }
+	client, err := docker.NewClient(host)
+	if err != nil {
+		return nil, err
+	}
 
-  copts := docker.CreateContainerOptions{
-    Config: &docker.Config{
-      Image: sopts.Image,
-      Cmd: []string{"/usr/bin/tail", "-f", "/dev/null"},
-      AttachStdin: true,
-    },
-  }
+	copts := docker.CreateContainerOptions{
+		Config: &docker.Config{
+			Image:       sopts.Image,
+			Cmd:         []string{"/usr/bin/tail", "-f", "/dev/null"},
+			AttachStdin: true,
+		},
+	}
 
-  container, err := client.CreateContainer(copts)
-  if err != nil {
-    return nil, err
-  }
+	container, err := client.CreateContainer(copts)
+	if err != nil {
+		return nil, err
+	}
 
-  s := &Session{client, container}
+	s := &Session{client, container}
 
-  err = s.start()
-  if err != nil {
-    s.Close()
-    return nil, err
-  }
+	err = s.start()
+	if err != nil {
+		s.Close()
+		return nil, err
+	}
 
-  return s, nil
+	return s, nil
 
 }
 
 func (s *Session) start() error {
-  return s.client.StartContainer(s.container.ID, nil)
+	return s.client.StartContainer(s.container.ID, nil)
 }
 
 func (s *Session) Run(c Cmd) error {
-  cmd := append([]string{c.Path}, c.Args...)
-  exec, err := s.client.CreateExec(docker.CreateExecOptions{
-    Container: s.container.ID,
-    Cmd: cmd,
-    AttachStdin: c.Stdin != nil,
-    AttachStdout: c.Stdout != nil,
-    AttachStderr: c.Stderr != nil,
-  })
-  if err != nil {
-    return err
-  }
-  return s.client.StartExec(exec.ID, docker.StartExecOptions{
-    InputStream: c.Stdin,
-    OutputStream: c.Stdout,
-    ErrorStream: c.Stderr,
-  })
+	cmd := append([]string{c.Path}, c.Args...)
+	exec, err := s.client.CreateExec(docker.CreateExecOptions{
+		Container:    s.container.ID,
+		Cmd:          cmd,
+		AttachStdin:  c.Stdin != nil,
+		AttachStdout: c.Stdout != nil,
+		AttachStderr: c.Stderr != nil,
+	})
+	if err != nil {
+		return err
+	}
+	return s.client.StartExec(exec.ID, docker.StartExecOptions{
+		InputStream:  c.Stdin,
+		OutputStream: c.Stdout,
+		ErrorStream:  c.Stderr,
+	})
 }
 
 func (s *Session) Save() (string, error) {
-  img, err := s.client.CommitContainer(docker.CommitContainerOptions{
-    Container: s.container.ID,
-  })
-  if err != nil {
-    return "", err
-  }
-  return img.ID, nil
+	img, err := s.client.CommitContainer(docker.CommitContainerOptions{
+		Container: s.container.ID,
+	})
+	if err != nil {
+		return "", err
+	}
+	return img.ID, nil
 }
 
 func (s *Session) Close() error {
-  opts := docker.RemoveContainerOptions{
-    ID: s.container.ID,
-    Force: true,
-  }
-  return s.client.RemoveContainer(opts)
+	opts := docker.RemoveContainerOptions{
+		ID:    s.container.ID,
+		Force: true,
+	}
+	return s.client.RemoveContainer(opts)
 }
